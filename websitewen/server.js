6264d41cca54d22bed12a95f87573271b9a73599
Catorge <kyle321550@gmail.com>
2024-04-25 12:12:03 +0200

    crappy GUI with arduino

--- websitewen/server.js
+++ websitewen/server.js
@@ -7,6 +7,12 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 const port = 3000;
+const path = require('path');
+
+app.use('/pictures', (express.static('./pictures')));
+app.use('/websitewen', (express.static('./websitewen')));
+app.use(express.static(path.join(__dirname, 'websitewen')));
+app.use('/', express.static(path.join(__dirname, 'pictures')));
 
 const parsers = SerialPort.parsers;
 const { ReadlineParser} = require("@serialport/parser-readline");
@@ -15,7 +21,7 @@ const parser = new ReadlineParser({ delimeter: "\r\n" });
 const fs = require('fs');
 //Arduino Serialport
 const portArduino = new SerialPort.SerialPort({
-    path: "COM8",
+    path: "COM9",
     baudRate: 9600,
     dataBits: 8,
     parity: 'none',
@@ -30,8 +36,35 @@ console.error('Serial port: ', err.message);
 portArduino.pipe(parser);
 console.log(io.sockets.version); // will print the version number of the socket.io client
 parser.on('data', (data) =>{
-    console.log('hij werkt!');
+    if(data.trim() === 'accepted'){
+        io.emit('accepted');
+    }
+    if(data.trim() === 'button1'){
+        io.emit('button1');
+    }
+    var datarrray = data.split(',');
+      
+    var key1 = datarrray[0];
+    var key2 = datarrray[1];
+
+      //io.emit('data', data);
+      io.emit('keypadData', {key1: key1, key2: key2});
+
+      if(data.trim() === 'ZERO'){
+        io.emit('erase');
+      }
+      if(data.trim() === 'enter'){
+        io.emit('enter');    
+  }
     });
+
+    //temporary JSON
+    app.get('/data', (req, res) => {
+        res.json(data1);
+      })
+    const jsonData = fs.readFileSync(path.join(__dirname, 'data.json'));
+    const data1 = JSON.parse(jsonData);
+    console.log(data1);
 
 // Serve the landing page
 app.get('/', (req, res) => {
@@ -43,6 +76,16 @@ app.get('/balance-inquiry', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+// Serve the Pin page
+app.get('/Pin', (req, res) => {
+    res.sendFile(__dirname + '/Pin.html');
+});
+
+// Serve the Select page
+app.get('/Select', (req, res) => {
+    res.sendFile(__dirname + '/Select.html');
+});
+
 // Handle form submission and API request
 app.get('/api/balance/:bankAccount/:pinCode', async (req, res) => {
     const { bankAccount, pinCode } = req.params;
@@ -52,7 +95,7 @@ app.get('/api/balance/:bankAccount/:pinCode', async (req, res) => {
         const data = await response.json();
 
         if (response.ok) {
-            res.json(data);
+           res.json(data);
         } else {
             // Handle incorrect pin code or bank account number
             res.status(400).json({ error: 'Incorrect pin code or bank account number.' });
