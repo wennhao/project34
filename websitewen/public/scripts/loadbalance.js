d0d4404bda8cc70baf03558492e147136c57d992
wen <1028905@hr.nl>
2024-05-01 04:01:24 +0200

    added main page to startt the program!

--- websitewen/public/scripts/loadbalance.js
+++ websitewen/public/scripts/loadbalance.js
@@ -2,7 +2,7 @@ window.onload = function() {
     const balance = localStorage.getItem('balance');
     if (balance) {
         document.getElementById('balanceDisplay').innerHTML = 
-            `Uw saldo is: €${balance}`;
+            `Uw saldo is: <br> €${balance}`;
     } else {
         document.getElementById('balanceDisplay').innerHTML = 
             "Geen balansinformatie beschikbaar.";
