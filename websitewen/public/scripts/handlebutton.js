e74bf31763cc43365455c38652a571237952389b
wennhao <superwenhao@gmail.com>
2024-05-06 23:27:25 +0200

    Button functionality now partly works!

--- websitewen/public/scripts/handlebutton.js
+++ websitewen/public/scripts/handlebutton.js
@@ -1,14 +1,85 @@
 var socket = io();
+
 socket.on('connect', function() {
     console.log('Connected to WebSocket server!');
 });
 
 socket.on('button1', function() {
-    console.log('Accepted event received, redirecting...');
-    window.location.replace('/saldo');
+    console.log('Received button1 event');
+    if (window.location.pathname.includes('/keuze')) {
+        // Als de gebruiker zich op de saldo pagina bevindt
+        console.log('Redirecting to choice screen...');
+        window.location.replace('/opnemen');
+    } else if (window.location.pathname.includes('/saldo')) {
+        // Als de gebruiker zich op de saldo pagina bevindt
+        console.log('Redirecting to keuze screen...');
+        window.location.replace('/saldo');
+    } else {
+        // Default actie als geen van bovenstaande van toepassing is
+        console.log('Redirecting to home...');
+        window.location.replace('/pin');
+    }
 });
 
-socket.on('button3', function(data) {
-    console.log('Redirecting back to choice screen...');
-    window.location.replace('/keuze');
+socket.on('button2', function() {
+    console.log('Accepted event received, redirecting...');
+    if (window.location.pathname.includes('/keuze')) {
+        // Als de gebruiker zich op de saldo pagina bevindt
+        console.log('Redirecting to snelopnemen screen...');
+        window.location.replace('/snelopnemen');
+    } else if (window.location.pathname.includes('/snelopnemen')) {
+        // Als de gebruiker zich op de keuze pagina bevindt
+        console.log('Redirecting to keuze screen...');
+        window.location.replace('/keuze');
+    } else if (window.location.pathname.includes('/saldo')) {
+        // Als de gebruiker zich op de saldo pagina bevindt
+        console.log('Redirecting to keuze screen...');
+        window.location.replace('/saldo');
+    } else {
+        // Default actie als geen van bovenstaande van toepassing is
+        console.log('Redirecting to home...');
+        window.location.replace('/pin');
+    };
+});
+
+socket.on('button3', function() {
+    console.log('Received button3 event');
+    if (window.location.pathname.includes('/keuze')) {
+        // Als de gebruiker zich op de saldo pagina bevindt
+        console.log('Redirecting to saldo screen...');
+        window.location.replace('/saldo');
+    } else if (window.location.pathname.includes('/saldo')) {
+        // Als de gebruiker zich op de keuze pagina bevindt
+        console.log('Redirecting to keuze screen...');
+        window.location.replace('/keuze');
+    } else if (window.location.pathname.includes('/opnemen')) {
+        // Als de gebruiker zich op de keuze pagina bevindt
+        console.log('Redirecting to keuze screen...');
+        window.location.replace('/keuze');
+    } else if (window.location.pathname.includes('/snelopnemen')) {
+        // Als de gebruiker zich op de keuze pagina bevindt
+        console.log('Redirecting to keuze screen...');
+        window.location.replace('/keuze');
+    } else {
+        // Default actie als geen van bovenstaande van toepassing is
+        console.log('Redirecting to home...');
+        window.location.replace('/pin');
+    }
+});
+
+socket.on('button4', function() {
+    console.log('Received button4 event');
+    if (window.location.pathname.includes('/opnemen')) {
+        // Als de gebruiker zich op de saldo pagina bevindt
+        console.log('Redirecting to saldo screen...');
+        window.location.replace('/keuze');
+    } else if (window.location.pathname.includes('/saldo')) {
+        // Als de gebruiker zich op de saldo pagina bevindt
+        console.log('Redirecting to keuze screen...');
+        window.location.replace('/saldo');
+    } else {
+        // Default actie als geen van bovenstaande van toepassing is
+        console.log('Redirecting to home...');
+        window.location.replace('/keuze');
+    }
 });
